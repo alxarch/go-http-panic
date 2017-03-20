0b5efd00abd9bd7810f957f1a4672bcb17a3ab00
Sigalas Alexandros <alxarch@gmail.com>
2017-03-20 21:44:51 +0200

    Handle panic handler panics

--- httpanic.go
+++ httpanic.go
@@ -43,53 +43,52 @@ func Panic(code int, message string) {
 	panic(httpanic{code, message})
 }
 
-type PanicHandler func(id int64, err interface{}, stacktrace []string, r *http.Request)
+type PanicHandler func(id int64, err interface{}, stacktrace []byte, r *http.Request)
 
 var logmu sync.Mutex
 
-func defaultPanicHandler(id int64, err interface{}, stacktrace []string, r *http.Request) {
+func defaultPanicHandler(id int64, err interface{}, stacktrace []byte, r *http.Request) {
 	logmu.Lock()
 	defer logmu.Unlock()
 	log.Printf("panic=%016x message = %v\n", id, err)
-	for _, line := range stacktrace {
-		log.Printf("panic=%016x %s", id, line)
-	}
+	// for _, line := range stacktrace {
+	// 	log.Printf("panic=%016x %s", id, line)
+	// }
 }
 
 var Middleware = PanicHandler(defaultPanicHandler).Middleware
 
+const StackSize = 8192
+
+func (h PanicHandler) handle(e interface{}, r *http.Request) int64 {
+	stack := make([]byte, StackSize)
+	stack = stack[:runtime.Stack(stack, false)]
+	id := rand.Int63()
+	h(id, e, stack, r)
+	return id
+
+}
 func (h PanicHandler) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			e := recover()
-			if e == nil {
-				return
-			}
-			if hp, ok := e.(httpanic); ok {
-				http.Error(w, hp.Error(), hp.Code)
-				return
-			}
-			id := rand.Int63()
-			var lines []string
-			for skip := 1; ; skip++ {
-				pc, file, line, ok := runtime.Caller(skip)
-				if !ok {
-					break
+			if e := recover(); e != nil {
+				defer func() {
+					if ee := recover(); ee != nil {
+						log.Printf("Panic handler for %v panicked: %v", e, ee)
+					}
+				}()
+				if hp, ok := e.(httpanic); ok {
+					// Handle http coded errors gracefully
+					http.Error(w, hp.Error(), hp.Code)
+				} else {
+					id := h.handle(e, r)
+					body := fmt.Sprintf(
+						"%s\n%016x",
+						http.StatusText(http.StatusInternalServerError),
+						id)
+					http.Error(w, body, http.StatusInternalServerError)
 				}
-				if file[len(file)-1] == 'c' {
-					continue
-				}
-				f := runtime.FuncForPC(pc)
-				s := fmt.Sprintf("%s:%d %s()\n", file, line, f.Name())
-				lines = append(lines, s)
 			}
-			h(id, e, lines, r)
-			body := fmt.Sprintf(
-				"%s\n%016x",
-				http.StatusText(http.StatusInternalServerError),
-				id,
-			)
-			http.Error(w, body, http.StatusInternalServerError)
 		}()
 		next.ServeHTTP(w, r)
 	})
